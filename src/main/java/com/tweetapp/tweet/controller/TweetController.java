4674f09b0018841584e6c8628cc78299a48686c8
AkshitaSri31 <114106930+AkshitaSri31@users.noreply.github.com>
2022-09-23 02:39:51 +0530

    Update TweetController.java

--- src/main/java/com/tweetapp/tweet/controller/TweetController.java
+++ src/main/java/com/tweetapp/tweet/controller/TweetController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.Generated;
 
 @RequestMapping(value = "/api/v1.0/tweets")
-@CrossOrigin("https://fsetweetappbackend.azurewebsites.net")
+@CrossOrigin(origins="https://fsetweetappbackend.azurewebsites.net")
 @RestController
 @Generated
 public class TweetController {
